96f654a42cf194b36035569a2a0fb1840e69427f
Aurimas Slapsys <aurimas@vinted.com>
2014-05-18 23:12:01 +0300

    Add tutorial to menu scene

--- scene_menu.lua
+++ scene_menu.lua
@@ -3,10 +3,35 @@ Gamestate = require "hump.gamestate"
 local scene = {}
 
 function scene:draw()
-    local w, h = love.window.getDimensions( )
-    love.graphics.setBackgroundColor(20, 20, 20)
-    love.graphics.setColor(255, 255, 255, 255)
-    love.graphics.printf( "Press Enter to start a game", w/2-250, h/2, 500, "center")
+  local w, h = love.window.getDimensions( )
+  love.graphics.setBackgroundColor(20, 20, 20)
+  love.graphics.setColor(255, 255, 255, 255)
+
+  scene.drawTutorial( w/2-100, h/2-40, "Things you need to collect.", "gold")
+  scene.drawTutorial( w/2-100, h/2-20, "Gives you air.", "air")
+  scene.drawTutorial( w/2-100, h/2, "Gives you power.", "power")
+  scene.drawTutorial( w/2-100, h/2+20, "Slows you down.", "scrap")
+  scene.drawTutorial( w/2-100, h/2+40, "Chases and drains your power.", "drone")
+
+  love.graphics.printf( "Use WASD or arrow keys to move.", w/2-250, h/2+70, 500, "center")
+
+  love.graphics.printf( "Press Enter to start a game", w/2-250, h/2+90, 500, "center")
+end
+
+function scene.drawTutorial(x, y, text, type)
+  if type == "air" then color = {100, 100, 255, 255}
+  elseif type == "power" then color = {100, 255, 100, 255}
+  elseif type == "drone" then color = {255, 100, 100, 255}
+  elseif type == "scrap" then color = {230, 230, 230, 255}
+  else  color = {255, 255, 100, 255} end
+
+  love.graphics.setColor(color)
+  love.graphics.circle("line", x, y, 5, 8)
+
+
+  love.graphics.setColor(255, 255, 255, 255)
+  love.graphics.printf( text, x+10, y-6, 500, "left")
+
 end
 
 function scene:keyreleased(key, code)
